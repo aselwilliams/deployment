5d20ff4145927ef04630c24b12d8eca3630cdcc3
Asel Williams <aselwilliams22@gmail.com>
2022-12-15 16:17:39 -0600

    adds req res functions

---
+++ public/main.js
@@ -0,0 +1,72 @@
+const baseUrl = 'http://localhost:8040'
+const form = document.querySelector('form')
+const name = document.querySelector('#name')
+const company = document.querySelector('#company')
+const email = document.querySelector('#email')
+const phone = document.querySelector('#phone')
+const container = document.querySelector('tbody')
+
+function renderUsers() {
+  container.innerHTML = ''
+
+res.data.map((el, i)=> {
+    return container.innerHTML += `
+    <tr name=${i}>
+        <td>${name}</td>
+        <td>${company}</td>
+        <td>${email}</td>
+        <td>${phone}</td>
+    </tr>
+    `
+})
+document.querySelectorAll('tr').forEach(element => {
+    const theIndexValue = element.getAttribute('name');
+
+    element.addEventListener('click', () => {
+        axios
+            .delete(`${baseUrl}/api/users/${theIndexValue}`)
+            .then(res => {
+                renderUsers(res)
+            })
+    })
+})
+}
+
+function submitHandler(e) {
+    e.preventDefault();
+
+    axios
+        .post(`${baseUrl}/api/users`, { 
+            name: name.value,
+            company: company.value,
+            email: email.value,
+            phone: phone.value })
+        .then(res => {
+            renderUsers(res)
+        })
+        .catch(err => {
+            name.value = ''
+            company.value = ''
+            email.value = ''
+            phone.value = ''
+
+            const notif = document.createElement('aside')
+            notif.innerHTML = `<p>${err.response.data}</p>
+            <button class="close">close</button>`
+            document.body.appendChild(notif)
+
+            document.querySelectorAll('.close').forEach(btn => {
+                btn.addEventListener('click', (e) => {
+                    e.target.parentNode.remove()
+                })
+            })
+        })
+}
+
+//get all users on initial load
+axios.get(`${baseUrl}/api/users`)
+    .then(res=> {
+        renderUsers(res)
+    })
+
+    form.addEventListener('submit', submitHandler)
--- server/server.js
+++ server/server.js
@@ -1,12 +1,51 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const app = express()
+const cors= require('cors')
+const {ROLLBAR_KEY}= process.env
+
+app.use(express.json())
+app.use(cors())
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: ROLLBAR_KEY,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello USER!')
 
 app.use(express.static('public'))
 //app.use(express.static(`${__dirname}/public))
 
+const users = [
+    {
+        name:"Camran",
+        company:"G5",
+        email:"camran@aol.com",
+        phone:"3031245698"
+    }
+]
+
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'../public/index.html'))
+})
+
+app.get('/api/users', (req, res)=> {
+    rollbar.info('Someone got the users list')
+    res.status(200).send(users)
+})
+
+app.delete('/api/users/:index', (req, res)=> {
+    const targetIndex = +req.params.index
+
+    users.splice(targetIndex, 1)
+    rollbar.critical(`Someone deleted ${users[targetIndex]}`)
+    res.status(200).send(users)
 })
 
 app.listen(8040, () => console.log(`Server running on port 8040`))
